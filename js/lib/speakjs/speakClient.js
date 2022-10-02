9f1245608883546a290f606f891a008d368ce597
CrazyMediumScout <33694155+Seamusmario@users.noreply.github.com>
2022-10-02 11:47:50 -0400

    reimplement /bonzikill, /bonzify, and lipsync from may 2022 bwr+

--- js/lib/speakjs/speakClient.js
+++ js/lib/speakjs/speakClient.js
@@ -124,6 +124,95 @@
 		}
 	};
 	
+	speak.playWithBonziObj = function (text, args, onended, onstart, bonzi) {
+		var source = audioContext.createBufferSource();
+
+		source.stopOld = source.stop;
+		source.stop = function() {
+			this.stopOld();
+			if (this.endTimeout) clearTimeout(this.endTimeout);
+		};
+
+		bonzi.source = source;
+		bonzi.gainNode = audioContext.createGain();
+		bonzi.source.connect(bonzi.gainNode).connect(audioContext.destination);
+		bonzi.analyser = audioContext.createAnalyser();
+		bonzi.gainNode.connect(bonzi.analyser); 
+
+		var PROFILE = 1;
+		
+		function startSource(source) {
+			if (source.start) {
+				source.start(0);
+			} else {
+				source.noteOn(0);
+			}
+			if (onstart) onstart(source);
+		}
+		
+		function playSound(streamBuffer) {
+			source.connect(audioContext.destination);
+			// since the ended-event isn't generally implemented,
+			// we need to use the decodeAudioData()-method in order
+			// to extract the duration to be used as a timeout-delay
+			audioContext.decodeAudioData(streamBuffer, function (audioData) {
+				// detect any implementation of the ended-event
+				// Chrome added support for the ended-event lately,
+				// but it's unreliable (doesn't fire every time)
+				// so let's exclude it.
+				if (!isChrome && source.onended !== undefined) {
+					// we could also use "source.addEventListener('ended', callback, false)" here
+					source.onended = onended;
+				} else {
+					var duration = audioData.duration;
+					// convert to msecs
+					// use a default of 1 sec, if we lack a valid duration
+					var delay = (duration) ? Math.ceil(duration * 1000) : 1000;
+					source.endTimeout = setTimeout(onended, delay);
+				}
+				// finally assign the buffer
+				source.buffer = audioData;
+				// start playback for Chrome >= 32
+				// please note that this would be without effect on iOS, since we're
+				// inside an async callback and iOS requires direct user interaction
+				if (chromeVersion >= 32) startSource(source)
+			},
+			function(error) { /* decoding-error-callback */ }
+		);
+			// normal start of playback, this would be essentially autoplay
+			// but is without any effect in Chrome 32
+			// let's exclude Chrome 32 and higher to avoid any double calls anyway
+			if (!isChrome || chromeVersion < 32) {
+				startSource(source);
+			}
+		}
+		
+		function handleWav(wav) {
+			var startTime = Date.now();
+			var buffer = new ArrayBuffer(wav.length);
+			new Uint8Array(buffer).set(wav);
+			// TODO: try playAudioDataAPI(data), and fallback if failed
+			playSound(buffer);
+			if (PROFILE) console.log('speak.js: wav processing took ' + (Date.now() - startTime).toFixed(2) + ' ms');
+		}
+		
+		if (args && args.noWorker) {
+			// Do everything right now. speakGenerator.js must have been loaded.
+			var startTime = Date.now();
+			var wav = generateSpeech(text, args);
+			if (PROFILE) console.log('speak.js: processing took ' + (Date.now() - startTime).toFixed(2) + ' ms');
+			playSound(wav);
+		} else {
+			// Call the worker, which will return a wav that we then play
+			var startTime = Date.now();
+			speakWorker.onmessage = function (event) {
+				if (PROFILE) console.log('speak.js: worker processing took ' + (Date.now() - startTime).toFixed(2) + ' ms');
+				handleWav(event.data);
+			};
+			speakWorker.postMessage({ text: text, args: args });
+		}
+	};
+	
 	// Expose speak to the global object
 	window.speak = speak;
 })(window);
